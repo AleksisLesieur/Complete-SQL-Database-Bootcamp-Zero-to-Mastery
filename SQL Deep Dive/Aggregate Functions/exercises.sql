7cd1ac13c4cde78d81a9db1f7c4f2bf1b40b1745
Aleksis Jonas Lesieur <69734158+AleksisLesieur@users.noreply.github.com>
2024-09-21 23:04:06 +0300

    Update exercises.sql
    did the exercise without googling! here are my answers

--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -8,11 +8,13 @@
 
 -- Question 1: What is the average salary for the company?
 -- Table: Salaries
-
+  select avg(salary) from salaries s 
+63810.744836143706
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
-
+  select max(birth_date) from employees e 
+1965-02-01
 
 -- BEFORE YOU START
 /*
@@ -23,7 +25,8 @@
 
 -- Question 1: How many towns are there in france?
 -- Table: Towns
-
+  select count(name) from towns t 
+36684
 
 -- BEFORE YOU START
 /*
@@ -34,6 +37,10 @@
 
 -- Question 1: How many official languages are there?
 -- Table: countrylanguage
+  select count(language) from countrylanguage c where isofficial is true 
+238
+
+
 
 -- Question 2: What is the average life expectancy in the world?
 -- Table: country
